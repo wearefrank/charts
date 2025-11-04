c620baf4d4090ab588019a10ed63f824124492ea
lenardvandermaas <51166193+lenardvandermaas@users.noreply.github.com>
2025-11-04 14:53:38 +0100

    Update pre-commit.js

--- .husky/pre-commit.js
+++ .husky/pre-commit.js
@@ -69,10 +69,8 @@ function lintChartFolder(folder) {
 function main() {
     console.log(`🔍 Check if changed folders are charts\n`);
     const changedFiles = getChangedFiles();
-    console.log('files', changedFiles);
     const changedFolders = getChangedFolders(changedFiles);
     for (const folder of changedFolders) {
-        console.log('folder', folder)
         if (isChartFolder(folder)) {
             updateChartDependencies(folder);
             generateChartReadmeAndSchema(folder);
